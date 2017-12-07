65413c23f92e87581f3d4556994a8b6d3acdec5c
limaa1 <anjinl@yahoo.com>
2017-12-07 14:53:28 -0500

    created the various api methods

--- src/app.py
+++ src/app.py
@@ -2,10 +2,9 @@ from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
-
 tokenIndex = {}
 
-@app.route("/add", methods=['GET', 'POST'])
+@app.route("/TeamRhino/add", methods=['GET', 'POST'])
 def add_ngrams():
 	if not request.json:
 		abort(400)
@@ -17,11 +16,19 @@ def add_ngrams():
 	return "Add successful!"
 
 
-@app.route("/")
+@app.route("/TeamRhino/stopwords", methods=['GET'])
+def get_stopwords():
+	return "List of stopwords"
+
+@app.route("/TeamRhino/tokens", methods=['GET', 'POST'])
+def get_token_metadata():
+	return "Documents associated with searched tokens"
+
+@app.route("/TeamRhino")
 def homepage():
 	return "Welcome to team Rhino's Indexing page"
 
 if __name__ =='__main__':
-	app.run(debug=True, host="0.0.0.0")
-	# app.run(host="0.0.0.0")
+	app.run(debug=True)
+	# app.run(debug=True, host="0.0.0.0")
 
