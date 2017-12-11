caef910bd0f16f06778f09bfc08eb890993a381d
limaa1 <anjinsemail@gmail.com>
2017-12-10 19:28:35 -0500

    added testfile

--- src/InvertedIndex_class.py
+++ src/InvertedIndex_class.py
@@ -19,6 +19,8 @@ class InvertedIndex:
 	def __init__(self):
 		self.indexstore = {}
 		self.stopwords = {}
+		self.documentstore = {}
+
 	"""
 	addMetadata
 
@@ -38,9 +40,11 @@ class InvertedIndex:
 					]
 				}
 				tokens: [
-					token: string,
-					ngramSize: int,
-					locations: [int]
+					{
+						token: string,
+						ngramSize: int,
+						locations: [int]
+					}
 				]
 			}
 
@@ -48,21 +52,53 @@ class InvertedIndex:
 
 	return: none
 	"""
-	def addMetadata(self, document):
+	def addMetadata(self, document, timestamp):
 		if not self.verifyJSON(document):
 			return "Incorrect submission format"
-		# return document['documentMetadata']['documentID']
 
-		for tokeninfo in document['tokens']:
-			if tokeninfo['token'] in self.indexstore:
-				self.updateIndex(tokeninfo['token'], document)
+		self.handleDocument(document['documentMetadata'],\
+			document['documentMetadata']['documentID'],\
+			timestamp)
+		self.handleTokens(document['tokens'],\
+			document['documentMetadata']['documentID'])
+
+		return "Add Successful | indexed the following tokens:\
+			    \n%s" %(' \n'.join([t['token'] for t in \
+			    				    document['tokens']]))
+
+	"""
+	handleDocument
+
+	params:
+		- metadata: json: 
+		- timestamp: float
+
+	- adds/updates a document to an index
+	"""
+	def handleDocument(self, metadata, docid, timestamp):
+		self.documentstore[docid] = (timestamp, metadata)
+
+
+	"""
+	handleTokens
+
+	params:
+		- tokens, list of strings
+		- docid, string
+
+	- iterates over tokens and adds/updates tokens to an index
+	"""
+	def handleTokens(self, tokens, docid):
+		for info in tokens:
+			if info['token'] in self.indexstore:
+				self.updateToken(info['token'],\
+					info['locations'],\
+					docid)
 			else:
-				self.indexstore[tokeninfo['token']] = \
-				self.createStore(document['documentMetadata'], \
-								 len(tokeninfo['locations'])
-								)
+				self.indexstore[info['token']] = \
+					self.createStore(info['locations'],\
+						docid)
 
-		return "Add Successful"
 
 	"""
 	createStore
@@ -73,8 +109,10 @@ class InvertedIndex:
 	return: list of word occurences and the first document metadata
 			associated
 	"""
-	def createStore(self, metadata, numOccurences):
-		return [numOccurences, metadata]
+	def createStore(self, occurences, docid):
+		# self.documentstore[docid] = [timestamp, metadata]
+		# print numOccurences
+		return [occurences, docid]
 
 	"""
 	updateIndex
@@ -83,15 +121,18 @@ class InvertedIndex:
 		- token: string
 		- metadata: metadata to update a token 
 	"""
-	def updateIndex(self, token, metadata):
+	def updateToken(self, token, metadata, timestamp):
 		# if metadata['documentID'] in self.indexstore[token]:
 
 		return None
 
 	"""
 	getStopwords
-
-	- returns a json list of the 50 most frequently occuring words
+	
+	- returns a json list of the 50 (or all words)
+	  most frequently occuring words
+	
+	return: json list
 	"""
 	def getStopwords(self):
 
@@ -119,21 +160,48 @@ class InvertedIndex:
 			}
 		  ],
 		  tokens: [
-			token: string,
-			ngramSize: int,
-			documentOccurences: [
-			  {
-				documentID: string,
-				locations: [int]
-			  }
-			]
+			{
+				token: string,
+				ngramSize: int,
+				documentOccurences: [
+				  {
+					documentID: string,
+					locations: [int]
+				  }
+				]
+			}
 		  ]
 		}
-	"""
-	def searchToken(self, token):
 
+	- json object has 
+		- `tokens`: a list of tokens and associated document stats
+		- returnCode: int, indicated the status of retrieval
+		- error: description of error if present
+		- documents: metadata concerning documents associated for the
+		             returned tokens
+
+	"""
+	def searchToken(self, tokens):
+		search = {'returnCode': 0, 'error': "", \
+		          'documents': [], 'tokens': []}
+
+		for token in tokens:
+			docinfo = self.findDocs(token)
+			indexinfo = {'token': token, ngramSize: len(token),\
+						 'documentOccurences': docinfo}
+			search['tokens'].append([indexinfo])
 		return None
 
+	"""
+	verifyJSON
+
+	params:
+		- document: json object
+
+	- confirms that the POSTed json contains documentMetadata and tokens
+
+	return: boolean, True if valid json else False
+	"""
 	def verifyJSON(self, document):
 		if not 'documentMetadata' in document:
 			return False
@@ -141,7 +209,18 @@ class InvertedIndex:
 			return False
 		return True
 
+	"""
+	findDocs
 
-# index = InvertedIndex()
-# index.addMetadata({'documentID': '1'})
+	params:
+		- token: string
 
+	- finds the associated documents for a given token
+	
+	return: list of dict of documentID and token locations
+	"""
+	def findDocs(self, token):
+		ret = []
+		for doc in self.invertedindex[token][1:]:
+			ret.append(doc)
+		return ret
---
+++ src/test.py
@@ -0,0 +1,72 @@
+import requests
+import json
+
+test_metadata1 = {
+	'documentMetadata': {
+		'documentID': 'testfile',
+		'wordCount': 50,
+	},
+	'tokens': [
+		{
+			'token': 'hello',
+			'ngramSize': 5,
+			'locations': [1,2,3,4]
+		},
+		{
+			'token': 'world',
+			'ngramSize': 5,
+			'locations': [5,6,7,8]
+		},
+		{
+			'token': 'what a day',
+			'ngramSize': 11,
+			'locations': [9,10,11,12]
+		},
+	]
+}
+
+test_metadata2 = {
+	'documentMetadata': {
+		'documentID': 'testfile2',
+		'wordCount': 50,
+	},
+	'tokens': [
+		{
+			'token': 'yeah',
+			'ngramSize': 5,
+			'locations': [1,2,3,4]
+		},
+		{
+			'token': 'okay',
+			'ngramSize': 5,
+			'locations': [5,6,7,8]
+		},
+		{
+			'token': "it's snowing",
+			'ngramSize': 11,
+			'locations': [9,10,11,12]
+		},
+		{
+			'token': 'hello',
+			'ngramSize': 5,
+			'locations': [1,2,3,4,15]
+		},
+	]
+}
+
+# res = requests.post('http://teamrhino.cs.rpi.edu:5000/TeamRhino/add', \
+				   # json=test_metadata)
+
+res = requests.post('http://127.0.0.1:5000/TeamRhino/add', \
+					json = test_metadata1)
+
+print 'response from server:', res.text
+
+res = requests.post('http://127.0.0.1:5000/TeamRhino/add', \
+					json = test_metadata2)
+
+print 'response from server:', res.text
+
+res = requests.get('http://127.0.0.1:5000/TeamRhino/stopwords')
+
+print 'response from server: [%s]' %(' '.join(json.loads(res.text)))
