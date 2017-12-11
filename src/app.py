1b592f02d1ce5a4491d9b6e45dda7110e6c68ada
limaa1 <anjinsemail@gmail.com>
2017-12-11 12:14:39 -0500

    fixed merge

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, abort
 from InvertedIndex_class import InvertedIndex
+import time
 
 app = Flask(__name__)
 
@@ -7,28 +8,27 @@ tokenIndex = InvertedIndex()
 
 @app.route("/TeamRhino/add", methods=['GET', 'POST'])
 def add_tokens():
+	t = time.time()
 	if not request.json:
 		abort(400)
-	return tokenIndex.addMetadata(request.json)
+	return tokenIndex.addMetadata(request.json, t)
 
 @app.route("/TeamRhino/stopwords", methods=['GET'])
 def get_stopwords():
-<<<<<<< HEAD
-	return tokenIndex.getStopwords()
-=======
-	return ["the", "of", "to", "a", "and", "in", "said", "for", "that", "was",
+	# return tokenIndex.getStopwords()
+	return jsonify(["the", "of", "to", "a", "and", "in", "said", "for", "that", "was",
 			"on", "he", "is", "with", "at", "by", "it", "from", "as", "be",
 			"were", "an", "have", "his", "but", "has", "are", "not", "who",
-			"they"]
->>>>>>> 9acfd17f8671e1e1de746a21710f1a3eb4bbcd78
+			"they"])
 
 @app.route("/TeamRhino/tokens", methods=['GET', 'POST'])
 def get_token_metadata():
-	return "Documents associated with searched tokens"
+	return tokenIndex.searchToken(request.json)
 
 @app.route("/TeamRhino")
 def homepage():
 	return "Welcome to team Rhino's Indexing page"
 
 if __name__ =='__main__':
-	app.run(debug=True, host="0.0.0.0")
+	app.run(debug=True)
+	# app.run(debug=True, host="0.0.0.0")
