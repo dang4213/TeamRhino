64c8ca37a0ade35640d983be1397fb4638ddf530
limaa1 <anjinsemail@gmail.com>
2017-12-12 10:47:36 -0500

    added comments to test.py

--- src/test.py
+++ src/test.py
@@ -4,7 +4,16 @@ import re
 from os import listdir
 from os.path import isfile, join
 
+"""
+cleanDocument
 
+- params:
+    - contents: string of file contents
+
+- removes any script tags, html tags, special chars, and punctuation
+
+return: list of strings
+"""
 def cleanDocument(contents):
 
     contents = re.sub('<script.+</script>', ' ', contents)
@@ -15,7 +24,17 @@ def cleanDocument(contents):
     contents = re.sub("(--|-)", ' ', contents).split()
     return contents
 
+"""
+getLocations
 
+- params:
+    - words: list of strings
+
+- iterates over an enumerated list of strings, adding the current index
+  to a hashmap noting the various indexes of each string
+
+return: hashmap
+"""
 def getLocations(words):
 
     word_locations = {}
@@ -26,7 +45,18 @@ def getLocations(words):
             word_locations[word] = [index]
     return word_locations
 
+"""
+createTestJSON
 
+- params:
+    - words: list of strings
+    - filename: string
+
+- create a JSON object with the given file name
+- sets the locations of each string to their found indexes
+
+return: JSON object
+"""
 def createTestJSON(words, filename):
 
     locations = getLocations(words)
@@ -41,30 +71,44 @@ def createTestJSON(words, filename):
                    for token in locations.keys()]}
     return testjson
 
+"""
+testAPI
 
-files = [file for file in listdir("./testfiles")
-         if isfile(join("./testfiles", file))]
+- params: none
 
-for filename in files:
-    with open("./testfiles/" + files[0]) as file:
-        contents = file.read().replace('\n', ' ')
+- browses the testfiles directory for all files
+- creates a JSON object for each file
+- sends the JSON to our add api and then requests
+  the first ten unique words and the stopwords
 
-    contents = cleanDocument(contents)
-    words = [word for word in contents if re.search('[a-zA-z]', word)]
+return: none
+"""
+def testAPI():
+    files = [file for file in listdir("./testfiles")
+             if isfile(join("./testfiles", file))]
 
-    testjson = createTestJSON(words, files[0])
+    for filename in files:
+        with open("./testfiles/" + files[0]) as file:
+            contents = file.read().replace('\n', ' ')
 
-    try:
-        res = requests.post('http://127.0.0.1:5000/TeamRhino/add',
-                            json=testjson)
-        print res.text
+        contents = cleanDocument(contents)
+        words = [word for word in contents if re.search('[a-zA-z]', word)]
 
-        testjson = {'tokens': [word for word in words[:10]]}
-        res = requests.get('http://127.0.0.1:5000/TeamRhino/tokens',
-                           json=testjson)
-        print res.text
+        testjson = createTestJSON(words, files[0])
 
-        res = requests.get('http://127.0.0.1:5000/TeamRhino/stopwords')
-        print res.text
-    except:
-        print "API request failed"
+        try:
+            res = requests.post('http://127.0.0.1:5000/TeamRhino/add',
+                                json=testjson)
+            print res.text
+
+            testjson = {'tokens': [word for word in words[:10]]}
+            res = requests.get('http://127.0.0.1:5000/TeamRhino/tokens',
+                               json=testjson)
+            print res.text
+
+            res = requests.get('http://127.0.0.1:5000/TeamRhino/stopwords')
+            print res.text
+        except:
+            print "API request failed"
+
+testAPI()
