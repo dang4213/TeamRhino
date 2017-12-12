4de3504b5814ee2f250c73568ad5e3797fa9d8dd
limaa1 <anjinsemail@gmail.com>
2017-12-12 10:36:34 -0500

    adjusted to pep8 standards

--- src/app.py
+++ src/app.py
@@ -6,25 +6,32 @@ app = Flask(__name__)
 
 tokenIndex = InvertedIndex()
 
+
 @app.route("/TeamRhino/add", methods=['GET', 'POST'])
 def add_tokens():
-	t = time.time()
-	if not request.json:
-		abort(400)
-	return tokenIndex.addMetadata(request.json, t)
+
+    t = time.time()
+    if not request.json:
+        abort(400)
+    return tokenIndex.addMetadata(request.json, t)
+
 
 @app.route("/TeamRhino/stopwords", methods=['GET'])
 def get_stopwords():
-	return tokenIndex.getStopwords()
+
+    return tokenIndex.getStopwords()
+
 
 @app.route("/TeamRhino/tokens", methods=['GET', 'POST'])
 def get_token_metadata():
-	return tokenIndex.searchToken(request.json)
+
+    return tokenIndex.searchToken(request.json)
+
 
 @app.route("/TeamRhino")
 def homepage():
-	return "Welcome to team Rhino's Indexing page"
 
-if __name__ =='__main__':
-	app.run(debug=True)
-	# app.run(debug=True, host="0.0.0.0")
+    return "Welcome to team Rhino's Indexing page"
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
--- src/test.py
+++ src/test.py
@@ -4,58 +4,67 @@ import re
 from os import listdir
 from os.path import isfile, join
 
+
 def cleanDocument(contents):
-	contents = re.sub('<script.+</script>', ' ', contents)
-	contents = re.sub('<[^<>]+>', ' ', contents)
-	contents = re.sub('&.+;', ' ', contents)
-	contents = re.sub("\d+", ' ', contents)
-	contents = re.sub("(,|\.|:|;|!|\(|\)|\?)", '', contents)
-	contents = re.sub("(--|-)", ' ', contents).split()
-	return contents
+
+    contents = re.sub('<script.+</script>', ' ', contents)
+    contents = re.sub('<[^<>]+>', ' ', contents)
+    contents = re.sub('&.+;', ' ', contents)
+    contents = re.sub("\d+", ' ', contents)
+    contents = re.sub("(,|\.|:|;|!|\(|\)|\?)", '', contents)
+    contents = re.sub("(--|-)", ' ', contents).split()
+    return contents
+
 
 def getLocations(words):
-	word_locations = {}
-	for index, word in enumerate(words):
-		if word in word_locations:
-			word_locations[word].append(index)
-		else:
-			word_locations[word] = [index]
-	return word_locations
+
+    word_locations = {}
+    for index, word in enumerate(words):
+        if word in word_locations:
+            word_locations[word].append(index)
+        else:
+            word_locations[word] = [index]
+    return word_locations
+
 
 def createTestJSON(words, filename):
-	locations = getLocations(words)
 
-	testjson = {
-		'documentMetadata': {
-			'documentID': filename,
-			'wordCount': len(locations)
-		},
-		'tokens': [{'token': token, 'ngramSize': len(token),
-					'locations': locations[token]} for token in locations.keys()]}
-	return testjson
+    locations = getLocations(words)
+
+    testjson = {
+        'documentMetadata': {
+            'documentID': filename,
+            'wordCount': len(locations)
+        },
+        'tokens': [{'token': token, 'ngramSize': len(token),
+                    'locations': locations[token]}
+                   for token in locations.keys()]}
+    return testjson
 
 
-files = [file for file in listdir("./testfiles") if isfile(join("./testfiles", file))]
+files = [file for file in listdir("./testfiles")
+         if isfile(join("./testfiles", file))]
 
 for filename in files:
-	with open("./testfiles/" + files[0]) as file:
-		contents = file.read().replace('\n', ' ')
+    with open("./testfiles/" + files[0]) as file:
+        contents = file.read().replace('\n', ' ')
 
-	contents = cleanDocument(contents)
-	words = [word for word in contents
-	         if re.search('[a-zA-z]', word)]
+    contents = cleanDocument(contents)
+    words = [word for word in contents if re.search('[a-zA-z]', word)]
 
-	testjson = createTestJSON(words, files[0])
+    testjson = createTestJSON(words, files[0])
 
-	try:
-		res = requests.post('http://127.0.0.1:5000/TeamRhino/add', json=testjson)
-		print res.text
+    try:
+        res = requests.post('http://127.0.0.1:5000/TeamRhino/add',
+                            json=testjson)
+        print res.text
 
-		testjson = {'tokens': [word for word in words[:10]]}
-		res = requests.get('http://127.0.0.1:5000/TeamRhino/tokens', json=testjson)
-		print res.text
+        testjson = {'tokens': [word for word in words[:10]]}
+        res = requests.get('http://127.0.0.1:5000/TeamRhino/tokens',
+                           json=testjson)
+        print res.text
 
-		res = requests.get('http://127.0.0.1:5000/TeamRhino/stopwords')
-		print res.text
-	except:
-		print "API request failed"
+        res = requests.get('http://127.0.0.1:5000/TeamRhino/stopwords')
+        print res.text
+    except:
+        print "API request failed"
